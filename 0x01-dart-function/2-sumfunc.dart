b30a1d8f564c60bc9fdb69c8e4f6efb3ac0d1847
Sirothpech <christophe.ngan@gmail.com>
2024-07-09 10:48:10 +0200

    Add: task 2

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,14 @@
+int add(int a, int b) {
+  return a + b;
+}
+
+int sub(int a, int b) {
+  return a - b;
+}
+
+String showFunc(int a, int b) {
+  String sum = "Add $a + $b = ${add(a, b)}";
+  String diff = "Sub $a - $b = ${sub(a, b)}";
+
+  return "$sum\n$diff";
+}
