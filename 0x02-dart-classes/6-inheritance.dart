a2632738b08837f879a9db142a329b1566c38937
Sirothpech <christophe.ngan@gmail.com>
2024-07-10 03:01:06 +0200

    Add: task 6

---
+++ 0x02-dart-classes/6-inheritance.dart
@@ -0,0 +1,37 @@
+import '6-password.dart';
+
+class User extends Password{
+  String? name;
+  int? age;
+  double? height;
+  int? id;
+  String? user_password;
+
+  User({this.id, this.name, this.age, this.height, this.user_password}) : super(password: user_password);
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "name": name,
+      "age": age,
+      "height": height,
+      "user_password": user_password,
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      age: json['age'],
+      height: json['height'],
+      user_password: json['user_password'],
+    );
+  }
+
+  @override
+      String toString() {
+        Password password = new Password(password: user_password);
+        return "User(id : $id ,name: $name, age: $age, height: $height, Password: ${password.isValid()})";
+    }
+}
