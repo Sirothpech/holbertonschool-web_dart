285078b6df78954493e061f229585ee09523290a
Sirothpech <christophe.ngan@gmail.com>
2024-07-10 02:31:02 +0200

    Add: task 3

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,21 @@
+class Password {
+    String _password = "";
+
+    Password({password}) {
+        this._password = password;
+    }
+
+    bool isValid() {
+        bool hasGoodLength = _password.length >= 8 && _password.length <= 16;
+        bool hasUppercase = _password.contains(new RegExp(r'[A-Z]'));
+        bool hasLowercase = _password.contains(new RegExp(r'[a-z]'));
+        bool hasDigits = _password.contains(new RegExp(r'[0-9]'));
+
+        return hasGoodLength && hasUppercase && hasLowercase && hasDigits;
+    }
+
+    @override
+    String toString() {
+        return ("Your Password is: $_password");
+    }
+}
